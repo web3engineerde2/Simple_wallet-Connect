961ed0a1a735a2b7d2d85f36ccdb948c580d8ac3
web3engineerde2 <philipp.guschl@gmx.de>
2024-03-23 23:44:26 +0100

    lol

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -19,7 +19,7 @@ import { motion } from "framer-motion";
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl xl:text-6xl lg:leading-normal font-extrabold'>
             <span className='text-transparent bg-clip-text bg-clig-text 
             bg-gradient-to-r from-red-500 to-orange-500 '>
-              Simple Wallet Connect {" "}
+              Simple Wallet Connectsddffsdf {" "}
             </span>
           <br />
       <TypeAnimation
